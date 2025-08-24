19ef6406d44f87d69ce25cc6e049f0a1ec0075b1
Palash <joshipalash80@gmail.com>
2025-08-25 04:21:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+//import express from "express "
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+        res.send("Twitterhere")
+})
+
+app.get('/login',(req,res)=>{
+    res.send("enter your login details")
+})
+ 
+app.get('/youtube',(req,res)=>{
+    res.send('<h2>chai aur Code </h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
