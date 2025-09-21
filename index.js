0725132caf5837e781b3dab7e0f78d73dfee7903
Palash <joshipalash80@gmail.com>
2025-09-21 23:54:44 +0530

    data added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 //import express from "express "
+
 require('dotenv').config()
 
 const app = express()
+
+const info={
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -18,6 +26,10 @@ app.get('/login',(req,res)=>{
  
 app.get('/youtube',(req,res)=>{
     res.send('<h2>chai aur Code </h2>')
+})
+
+app.get('/github',(req,res)=>{
+  res.json(info)
 })
 
 app.listen(process.env.PORT, () => {
